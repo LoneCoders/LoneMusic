fcbc651625a80f3364a425dae561a8aa9cf099d1
dev-sudharshan <gokulrajan01234@gmail.com>
2020-09-13 20:54:54 +0530

    added bottom bar and menu item

--- app/src/main/java/com/lonecoders/musicplayer/MainActivity.kt
+++ app/src/main/java/com/lonecoders/musicplayer/MainActivity.kt
@@ -2,10 +2,23 @@ package com.lonecoders.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayout
+import com.lonecoders.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Used dataBinding
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        MenuInflater(applicationContext).inflate(R.menu.menu,menu)
+        return true
     }
 }
