5fa87b27b14fafbd467e3578654a2d11ef3fd34d
dev-sudharshan <gokulrajan01234@gmail.com>
2020-09-14 14:16:22 +0530

    about fragment in progress

--- app/src/main/java/com/lonecoders/musicplayer/MainActivity.kt
+++ app/src/main/java/com/lonecoders/musicplayer/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.lonecoders.musicplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.tabs.TabLayout
 import com.lonecoders.musicplayer.databinding.ActivityMainBinding
@@ -12,15 +14,27 @@ import com.lonecoders.musicplayer.util.ViewPagerAdapter
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         binding.pager.adapter = ViewPagerAdapter(supportFragmentManager)
         binding.tabLayout.setupWithViewPager(binding.pager)
-
+        binding.toolbar.apply {
+            inflateMenu(R.menu.menu)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.about -> {
+                        Intent(applicationContext, AboutActivity::class.java).apply {
+                            startActivity(
+                                this
+                            )
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        MenuInflater(applicationContext).inflate(R.menu.menu,menu)
-        return true
-    }
+
 }
