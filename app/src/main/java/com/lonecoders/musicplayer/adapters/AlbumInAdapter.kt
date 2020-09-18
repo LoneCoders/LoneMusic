d13c7e2bf5f36f333c246c79f9238ac3036f5d9a
Dhina17 <dhinalogu@gmail.com>
2020-09-18 22:03:46 +0530

    Improve AlbumIn UI little bit

--- app/src/main/java/com/lonecoders/musicplayer/adapters/AlbumInAdapter.kt
+++ app/src/main/java/com/lonecoders/musicplayer/adapters/AlbumInAdapter.kt
@@ -3,6 +3,7 @@ package com.lonecoders.musicplayer.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lonecoders.musicplayer.R
@@ -19,8 +20,14 @@ class AlbumInAdapter(private val songsSet: Set<Song>) :
     }
 
     override fun onBindViewHolder(holder: SongListViewHolder, position: Int) {
-        holder.itemView.findViewById<TextView>(R.id.album_in_song).text =
+        holder.itemView.findViewById<TextView>(R.id.album_in_song_name).text =
             songsSet.elementAt(position).songName
+        val songInfo =
+            "${songsSet.elementAt(position).artistName} - ${songsSet.elementAt(position).albumName}"
+        holder.itemView.findViewById<TextView>(R.id.song_info).text = songInfo
+        if (songsSet.elementAt(position).songAlbumCover != null)
+            holder.itemView.findViewById<ImageView>(R.id.album_cover_in_song)
+                .setImageBitmap(songsSet.elementAt(position).songAlbumCover)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/lonecoders/musicplayer/fragments/AlbumInFragment.kt
+++ app/src/main/java/com/lonecoders/musicplayer/fragments/AlbumInFragment.kt
@@ -1,10 +1,11 @@
 package com.lonecoders.musicplayer.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lonecoders.musicplayer.R
@@ -23,10 +24,16 @@ class AlbumInFragment(private val albumSongs: MutableList<Song>) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val albumCoverHeadView = view.findViewById<ImageView>(R.id.album_in_cover_header)
+        if (albumSongs.toSet().first().songAlbumCover != null)
+            albumCoverHeadView.apply {
+                this.setImageBitmap(albumSongs.toSet().first().songAlbumCover)
+            }
+
 
         view.findViewById<RecyclerView>(R.id.album_in_songs_list).apply {
             setHasFixedSize(true)
-            layoutManager= LinearLayoutManager(requireContext())
+            layoutManager = LinearLayoutManager(requireContext())
             adapter = AlbumInAdapter(albumSongs.toSet())
         }
     }
