ae640edf0b3cf5e42c7bf02cfe0b0a26beaf14a6
Dhina17 <dhinalogu@gmail.com>
2021-06-07 22:17:45 +0530

    fragment: Replace tabs with bottom navigation view

--- app/src/main/java/com/lonecoders/musicplayer/fragments/PagerFragment.kt
+++ app/src/main/java/com/lonecoders/musicplayer/fragments/PagerFragment.kt
@@ -7,8 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.viewpager2.widget.ViewPager2
 import com.lonecoders.musicplayer.R
 import com.lonecoders.musicplayer.activities.AboutActivity
 import com.lonecoders.musicplayer.adapters.ViewPager2Adapter
@@ -17,25 +16,59 @@ import com.lonecoders.musicplayer.databinding.FragmentPagerBinding
 
 class PagerFragment : Fragment() {
 
-    lateinit var binding : FragmentPagerBinding
+    /**
+     * Data Binding instance
+     */
+    private lateinit var binding: FragmentPagerBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_pager, container, false)
+    ): View {
+        /**
+         * Inflate the layout for this fragment
+         */
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_pager,
+            container,
+            false
+        )
 
+        /**
+         * Set the adapter for ViewPager
+         */
         binding.pager.adapter = ViewPager2Adapter(this)
-        TabLayoutMediator(binding.tabLayout,binding.pager){tab: TabLayout.Tab, position: Int ->
-            tab.text = when(position){
-                0 -> "Songs"
-                1 -> "Album"
-                //2 -> "Artist"
-                2 -> "Playlists"
-                else -> ""
-            }
 
-        }.attach()
+        /**
+         * Bottom Navigation View
+         * Change the current page of the viewpager with selected item
+         */
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            val currentPage = when (it.itemId) {
+                R.id.songs_dest -> 0
+                R.id.albums_dest -> 1
+                R.id.playlist_dest -> 2
+                else -> 0
+            }
+            binding.pager.currentItem = currentPage
+
+            return@setOnNavigationItemSelectedListener true
+        }
+
+        /**
+         * Update the bottom navigation view item checked status after swiping in viewpager
+         */
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+
+        /**
+         * Toolbar menu items handling
+         */
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.about -> {
